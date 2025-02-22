b79bd3cdbe4f8ee5603e8d69245ccae8c6c1dfb4
dev78ashish <dev78ashish@gmail.com>
2025-02-22 23:20:19 +0530

    .

--- src/App.js
+++ src/App.js
@@ -22,7 +22,6 @@ const App = () => {
         />
         <Routes>
           <Route exact path="/" element={<News setProgress={setProgress} key="general" pageSize={pageSize} country="us" category="general" />} />
-          <Route exact path="/general" element={<News setProgress={setProgress} key="general" pageSize={pageSize} country="us" category="general" />} />
           <Route exact path="/business" element={<News setProgress={setProgress} key="business" pageSize={pageSize} country="us" category="business" />} />
           <Route exact path="/entertainment" element={<News setProgress={setProgress} key="entertainment" pageSize={pageSize} country="us" category="entertainment" />} />
           <Route exact path="/health" element={<News setProgress={setProgress} key="health" pageSize={pageSize} country="us" category="health" />} />
